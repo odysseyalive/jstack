b1f65f3bd03f9eda774495a74af9972c8496c7ca
Francis Meetze <francis@odysseyalive.com>
2025-09-15 02:16:41 -0700

    init script

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -4,40 +4,35 @@ set -e
 echo "Creating Supabase database users..."
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    -- Create users
     CREATE USER supabase_auth_admin WITH PASSWORD '$SUPABASE_PASSWORD';
     CREATE USER authenticator WITH PASSWORD '$SUPABASE_PASSWORD';
     CREATE USER postgres WITH SUPERUSER PASSWORD '$SUPABASE_PASSWORD';
+    
+    -- Create basic roles
     CREATE ROLE anon NOLOGIN NOINHERIT;
     CREATE ROLE authenticated NOLOGIN NOINHERIT;
     CREATE ROLE service_role NOLOGIN NOINHERIT BYPASSRLS;
     
-    -- Create required schemas
-    CREATE SCHEMA IF NOT EXISTS auth;
+    -- Create basic schemas
+    CREATE SCHEMA IF NOT EXISTS auth AUTHORIZATION supabase_auth_admin;
     CREATE SCHEMA IF NOT EXISTS storage;
     CREATE SCHEMA IF NOT EXISTS realtime;
     CREATE SCHEMA IF NOT EXISTS _realtime;
     CREATE SCHEMA IF NOT EXISTS graphql_public;
     
-    -- Grant schema permissions
-    GRANT ALL PRIVILEGES ON SCHEMA public TO supabase_auth_admin;
-    GRANT ALL PRIVILEGES ON SCHEMA auth TO supabase_auth_admin;
-    GRANT ALL PRIVILEGES ON SCHEMA storage TO supabase_auth_admin;
-    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO supabase_auth_admin;
-    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO supabase_auth_admin;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO supabase_auth_admin;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO supabase_auth_admin;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT ALL ON TABLES TO supabase_auth_admin;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT ALL ON SEQUENCES TO supabase_auth_admin;
+    -- Grant broad permissions
+    GRANT ALL ON SCHEMA public TO supabase_auth_admin, authenticator;
+    GRANT ALL ON SCHEMA auth TO supabase_auth_admin, authenticator;
+    GRANT ALL ON SCHEMA storage TO supabase_auth_admin, authenticator;
+    GRANT ALL ON ALL TABLES IN SCHEMA public TO supabase_auth_admin, authenticator;
+    GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO supabase_auth_admin, authenticator;
     
-    GRANT ALL PRIVILEGES ON SCHEMA public TO authenticator;
-    GRANT ALL PRIVILEGES ON SCHEMA auth TO authenticator;
-    GRANT ALL PRIVILEGES ON SCHEMA storage TO authenticator;
-    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO authenticator;
-    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO authenticator;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO authenticator;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO authenticator;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT ALL ON TABLES TO authenticator;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT ALL ON SEQUENCES TO authenticator;
+    -- Set default privileges
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO supabase_auth_admin, authenticator;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT ALL ON TABLES TO supabase_auth_admin, authenticator;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO supabase_auth_admin, authenticator;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT ALL ON SEQUENCES TO supabase_auth_admin, authenticator;
     
     -- Grant roles to authenticator
     GRANT anon TO authenticator;
