4e65f50bb7fccf8411691a5f9d5d0446f0784397
Francis Meetze <francis@odysseyalive.com>
2025-09-15 02:04:27 -0700

    supabase init script patch

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -8,18 +8,58 @@ echo "Creating Supabase database users..."
 
 # Create supabase_auth_admin user
 psql -v ON_ERROR_STOP=1 --username supabase_admin --dbname postgres <<-EOSQL
-    CREATE USER IF NOT EXISTS supabase_auth_admin WITH PASSWORD '${SUPABASE_PASSWORD}';
-    CREATE USER IF NOT EXISTS authenticator WITH PASSWORD '${SUPABASE_PASSWORD}';
-    CREATE USER IF NOT EXISTS postgres WITH SUPERUSER PASSWORD '${SUPABASE_PASSWORD}';
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'supabase_auth_admin') THEN
+            CREATE USER supabase_auth_admin WITH PASSWORD '${SUPABASE_PASSWORD}';
+        END IF;
+    END
+    \$\$;
+    
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'authenticator') THEN
+            CREATE USER authenticator WITH PASSWORD '${SUPABASE_PASSWORD}';
+        END IF;
+    END
+    \$\$;
+    
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'postgres') THEN
+            CREATE USER postgres WITH SUPERUSER PASSWORD '${SUPABASE_PASSWORD}';
+        END IF;
+    END
+    \$\$;
     
     -- Grant necessary permissions
     GRANT ALL PRIVILEGES ON DATABASE postgres TO supabase_auth_admin;
     GRANT ALL PRIVILEGES ON DATABASE postgres TO authenticator;
     
     -- Create roles for Supabase
-    CREATE ROLE IF NOT EXISTS anon NOLOGIN NOINHERIT;
-    CREATE ROLE IF NOT EXISTS authenticated NOLOGIN NOINHERIT;
-    CREATE ROLE IF NOT EXISTS service_role NOLOGIN NOINHERIT BYPASSRLS;
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'anon') THEN
+            CREATE ROLE anon NOLOGIN NOINHERIT;
+        END IF;
+    END
+    \$\$;
+    
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'authenticated') THEN
+            CREATE ROLE authenticated NOLOGIN NOINHERIT;
+        END IF;
+    END
+    \$\$;
+    
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'service_role') THEN
+            CREATE ROLE service_role NOLOGIN NOINHERIT BYPASSRLS;
+        END IF;
+    END
+    \$\$;
     
     -- Grant permissions to authenticator
     GRANT anon TO authenticator;
