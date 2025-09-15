5d2f6436e76ad56b090f22001aeb2d3b4dd2dc77
Francis Meetze <francis@odysseyalive.com>
2025-09-15 02:12:48 -0700

    script patch

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -11,18 +11,33 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE ROLE authenticated NOLOGIN NOINHERIT;
     CREATE ROLE service_role NOLOGIN NOINHERIT BYPASSRLS;
     
+    -- Create required schemas
+    CREATE SCHEMA IF NOT EXISTS auth;
+    CREATE SCHEMA IF NOT EXISTS storage;
+    CREATE SCHEMA IF NOT EXISTS realtime;
+    CREATE SCHEMA IF NOT EXISTS _realtime;
+    CREATE SCHEMA IF NOT EXISTS graphql_public;
+    
     -- Grant schema permissions
     GRANT ALL PRIVILEGES ON SCHEMA public TO supabase_auth_admin;
+    GRANT ALL PRIVILEGES ON SCHEMA auth TO supabase_auth_admin;
+    GRANT ALL PRIVILEGES ON SCHEMA storage TO supabase_auth_admin;
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO supabase_auth_admin;
     GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO supabase_auth_admin;
     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO supabase_auth_admin;
     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO supabase_auth_admin;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT ALL ON TABLES TO supabase_auth_admin;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT ALL ON SEQUENCES TO supabase_auth_admin;
     
     GRANT ALL PRIVILEGES ON SCHEMA public TO authenticator;
+    GRANT ALL PRIVILEGES ON SCHEMA auth TO authenticator;
+    GRANT ALL PRIVILEGES ON SCHEMA storage TO authenticator;
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO authenticator;
     GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO authenticator;
     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO authenticator;
     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO authenticator;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT ALL ON TABLES TO authenticator;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT ALL ON SEQUENCES TO authenticator;
     
     -- Grant roles to authenticator
     GRANT anon TO authenticator;
